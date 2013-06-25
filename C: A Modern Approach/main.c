b5518a9f429ee60a8bfb8ee3bab8760db71149be
Keith <user@Keiths-Macbook-Pro.local>
2013-06-24 17:05:15 -0700

    Initial Commit

---
+++ C: A Modern Approach/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  C: A Modern Approach
+//
+//  Created by Keith on 6/24/13.
+//  Copyright (c) 2013 Appology. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
